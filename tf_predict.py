0fdff20d6e44f04ea0907efa688a385740a455d9
Nathanael Vito <nathanael.vito@ti.ukdw.ac.id>
2023-12-11 16:57:52 +0700

    Add new emission carbon and nutrition feature

--- tf_predict.py
+++ tf_predict.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 # Assigning label names to the corresponding indexes
@@ -21,24 +22,16 @@ labels = {
     10: 'Vegetable-Fruit'
 }
 
+#load model
 model = load_model('D:\Dunia Perkuliahan\Semester 5\Bangkit\Model\model_tf.h5')
 
 # Loading images and their predictions
-prediction = []
-original = []
-image = []
-count = 0
 path_eval = "D:\Dunia Perkuliahan\Semester 5\Bangkit\Food_Image_Recognition\Dataset\evaluation"
 
-# for i in os.listdir(path_eval):
-#     for item in os.listdir(os.path.join(path_eval, i)):
-
 # code to open the image
-img = PIL.Image.open("D:\Dunia Perkuliahan\Semester 5\Bangkit\Food_Image_Recognition\Test Model\\sego.jpg")
+img = PIL.Image.open("D:\Dunia Perkuliahan\Semester 5\Bangkit\Food_Image_Recognition\Test Model\\roti.jpg")
 # resizing the image to (256,256)
 img = img.resize((256, 256))
-# appending image to the image list
-image.append(img)
 # converting image to array
 img = np.asarray(img, dtype=np.float32)
 # normalizing the image
@@ -49,16 +42,32 @@ img = img.reshape(-1, 256, 256, 3)
 predict = model.predict(img)
 # getting the index corresponding to the highest value in the prediction
 predict = np.argmax(predict)
-# appending the predicted class to the list
-prediction.append(labels[predict])
-# appending original class to the list
-original.append("Rice")
 
-# Visualizing the results
-# fig=plt.figure(figsize = (100,100))
 
-print("Prediction: " + prediction[0])
-# plt.xlabel("Prediction -" + prediction[0] +"   Original -" + original[0])
-# plt.imshow(image[0])
-# fig.tight_layout()
-# plt.show()
+# Read data from csv
+df = pd.read_csv('Carbon_Emission_of_Foods.csv')
+df2 = pd.read_csv('food_nutrition.csv')
+
+# Get the predicted class from the model
+pred = labels[predict]
+
+# Filter the DataFrame based on the predicted class
+filter_carbon = df[df['id'] == pred.lower()]
+filter_nutrition = df2[df2['name'] == pred.lower()]
+
+# Extract the 'value' for the predicted class
+carbon = filter_carbon['value'].values[0]
+protein = filter_nutrition['protein'].values[0]
+calcium = filter_nutrition['calcium'].values[0]
+fat = filter_nutrition['fat'].values[0]
+carbohydrates = filter_nutrition['carbohydrates'].values[0]
+vitamins = filter_nutrition['vitamins'].values[0]
+
+#print output
+print("Prediction: " + pred)
+print(f'Carbon Emission: {carbon:.2f} kg CO2')
+print(f'Protein: {protein:.2f} g')
+print(f'Calcium: {calcium:.2f} mg')
+print(f'Fat: {fat:.2f} g')
+print(f'Carbohydrates: {carbohydrates:.2f} g')
+print(f'Vitamins: {vitamins}')
